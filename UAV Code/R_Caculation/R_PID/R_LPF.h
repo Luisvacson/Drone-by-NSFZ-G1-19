18f43a125824f412c673f7aeab513e44d648f807
SpindriftXStar <3077448748@qq.com>
2023-01-09 22:56:23 +0800

    LPF and PID (Unfinished)

--- UAV Code/R_Caculation/R_PID/R_LPF.h
+++ UAV Code/R_Caculation/R_PID/R_LPF.h
@@ -5,7 +5,17 @@
 
 namespace R_LPF
 {
-    inline constexpr float calculate_lpf_alpha(float delta_time,float frequency);
+    //typedef struct _R_LPF
+    //{
+    //    float delta_time;
+    //    float frequency;
+    //}R_LPF_info;
+    //class R_LPF_Calculate
+    //{
+    //    public
+    //};
+    //inline constexpr float get_lpf_alpha(float delta_time,float frequency);
+    //inline constexpr float calculate_lpf(float input,float last,R_LPF_info info);
 };
 
 #endif
