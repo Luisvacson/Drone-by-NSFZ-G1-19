d5ab6e4a2cbd71595be71c49309df4dd3b3f7bd4
SpindriftXStar <3077448748@qq.com>
2023-02-26 18:14:48 +0800

    Script

--- UAV Code/Script/GenerateEquation.m
+++ UAV Code/Script/GenerateEquation.m
@@ -4,6 +4,8 @@ disp('Derivation start');
 
 %%state vector
 
+disp('Define state vector');
+
 syms q1 q2 q3 q4 'real' %state quaternion
 syms velocity_n velocity_e velocity_d 'real' %velocity in NED
 syms postion_n postion_e postion_d 'real' %position in NED
@@ -15,6 +17,10 @@ syms baro_bias 'real'%baro bias - m
 
 state_vector = [q1;q2;q3;q4;velocity_n;velocity_e;velocity_d;postion_n;postion_e;postion_d;omega_bias_x;omega_bias_y;omega_bias_z;acc_bias_x;acc_bias_y;acc_bias_z;mag_n;mag_e;mag_d;mag_bias_x;mag_bias_y;mag_bias_z;baro_bias];
 nstate = numel(state_vector);
+
+disp('Complete');
+
+disp('Define process equation');
 
 syms omega_x omega_y omega_z 'real' %omega measurement in XYZ - rad/s
 syms acc_x acc_y acc_z 'real' %acc measurement in XYZ - m/s^2
@@ -89,25 +95,58 @@ baro_bias_new = baro_bias + baro_bias_noise;
 
 process_equation = [quat_new;v_new;p_new;omega_bias_new;acc_bias_new;mag_n_new;mag_e_new;mag_d_new;mag_bias_x_new;mag_bias_y_new;mag_bias_z_new;baro_bias_new];
 
-%%generate equations
+disp('Complete');
 
-%derive the state transition matrix
+%%generate equations
+disp('Derive the state transition matrix');
+
 F = jacobian(process_equation,state_vector);
+
+disp('Set noise to 0');
+
 F = subs(F,{omega_noise_x,omega_noise_y,omega_noise_z,acc_noise_x,acc_noise_y,acc_noise_z,mag_noise_n,mag_noise_e,mag_noise_d,omega_bias_noise_x,omega_bias_noise_y,omega_bias_noise_z,acc_bias_noise_x,acc_bias_noise_y,acc_bias_noise_z,mag_bias_noise_x,mag_bias_noise_y,mag_bias_noise_z,baro_bias_noise},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
+
+disp('Optimise algebra');
+
 [F,OF] = OptimiseAlgebra(F,'OF');
 
-%define the control (disturbance) vector
+disp('Complete');
+
+disp('Define the control vector');
+
 dist_vector = [omega_noise_x;omega_noise_y;omega_noise_z;acc_noise_x;acc_noise_y;acc_noise_z;omega_bias_noise_x;omega_bias_noise_y;omega_bias_noise_z;acc_bias_noise_x;acc_bias_noise_y;acc_bias_noise_z;mag_noise_n;mag_noise_e;mag_noise_d;mag_bias_noise_x;mag_bias_noise_y;mag_bias_noise_z;baro_bias_noise];
 
-%derive the control(disturbance) influence matrix
+disp('Complete');
+
+disp('Derive the control influence matrix');
+
 G = jacobian(process_equation,dist_vector);
+
+disp('Set noise to 0');
+
 G = subs(G,{omega_noise_x,omega_noise_y,omega_noise_z,acc_noise_x,acc_noise_y,acc_noise_z,mag_noise_n,mag_noise_e,mag_noise_d,omega_bias_noise_x,omega_bias_noise_y,omega_bias_noise_z,acc_bias_noise_x,acc_bias_noise_y,acc_bias_noise_z,mag_bias_noise_x,mag_bias_noise_y,mag_bias_noise_z,baro_bias_noise},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
+
+disp('Optimise algebra');
+
 [G,OG] = OptimiseAlgebra(G,'OG');
 
-%derive the state error matrix
+disp('Complete');
+
+disp('Derive the state error matrix');
+
 noise = diag([omega_cov_x omega_cov_y omega_cov_z acc_cov_x acc_cov_y acc_cov_z  omega_bias_cov_x omega_bias_cov_y omega_bias_cov_z acc_bias_cov_x acc_bias_cov_y acc_bias_cov_z mag_noise_cov_n mag_noise_cov_e mag_noise_cov_d mag_bias_noise_cov_x mag_bias_noise_cov_y mag_bias_noise_cov_z baro_bias_noise_cov]);
+
+disp('Complete');
+
+disp('Calculate Q');
+
 Q = G*noise*transpose(G);
+
+disp('Optimise algebra');
+
 [Q,OQ] = OptimiseAlgebra(Q,'OQ');
+
+disp('Complete');
 
 %define a symbolic covariance matrix using strings to represent 
 %'_lp_' to represent '( '
@@ -121,10 +160,18 @@ for rowIndex = 1:nstate
     end
 end
 
+disp('Calculate PP');
+
 PP = F*P*transpose(F) + Q;
+
+disp('Optimise algebra');
+
 [PP,OPP] = OptimiseAlgebra(PP,'OPP');
 
-%%derive equations for sequential fusion of velocity and position measurements
+disp('Complete');
+
+%%derive equations for sequential fusion
+disp('Derive equations for sequential fusion');
 
 H_VN = jacobian(velocity_n,state_vector);
 K_VN = (P*transpose(H_VN))/(H_VN*P*transpose(H_VN) + rv_n);
@@ -151,7 +198,7 @@ K_VP = [K_VN,K_VE,K_VD,K_PN,K_PE,K_PD];
 clear   K_VN K_VE K_VD K_PN K_PE K_PD;
 [K_VP,OK_VP] = OptimiseAlgebra(K_VP,'OK_VP');
 
-%%derive equations for fusion of magnetic field measurement
+%derive equations for fusion of magnetic field measurement
 mag_measurement = transpose(tbn)*[mag_n;mag_e;mag_d] + [mag_bias_x;mag_bias_y;mag_bias_z]; % predicted measurement
 H_MAG = jacobian(mag_measurement,state_vector); % measurement Jacobian
 [H_MAG,OH_MAG] = OptimiseAlgebra(H_MAG,'OH_MAG');
@@ -163,32 +210,34 @@ K_MY = (P*transpose(H_MAG(2,:)))/(H_MAG(2,:)*P*transpose(H_MAG(2,:)) + rmag_y); 
 K_MZ = (P*transpose(H_MAG(3,:)))/(H_MAG(3,:)*P*transpose(H_MAG(3,:)) + rmag_z); % Kalman gain vector
 [K_MZ,OK_MZ] = OptimiseAlgebra(K_MZ,'OK_MZ');
 
-%%derive equations for fusion of magnetic declination measurement
+%derive equations for fusion of magnetic declination measurement
 dec_measurement = atan(mag_e/mag_n);
 H_DEC = jacobian(dec_measurement,state_vector);
 [H_DEC,OH_DEC] = OptimiseAlgebra(H_DEC,'OH_DEC');
 K_DEC = (P*transpose(H_DEC))/(H_DEC*P*transpose(H_DEC) + rdec);
 [K_DEC,OK_DEC] = OptimiseAlgebra(K_DEC,'OK_DEC');
 
-%%derive equations for fusion of yaw (yaw321)
+%derive equations for fusion of yaw (yaw321)
 yaw321_measurement = atan(tbn(2,1)/tbn(1,1));
 H_YAW321 = jacobian(yaw321_measurement,state_vector);
 H_YAW321 = simplify(H_YAW321);
 K_YAW321 = (P*transpose(H_YAW321))/(H_YAW321*P*transpose(H_YAW321) + ryaw);
 K_YAW321 = simplify(K_YAW321);
 
-%%derive equations for fusion of yaw (yaw312)
+%derive equations for fusion of yaw (yaw312)
 yaw312_measurement = atan(-tbn(1,2)/tbn(2,2));
 H_YAW312 = jacobian(yaw312_measurement,state_vector);
 H_YAW312 = simplify(H_YAW312);
 K_YAW312 = (P*transpose(H_YAW312))/(H_YAW312*P*transpose(H_YAW312) + ryaw);
 K_YAW312 = simplify(K_YAW312);
 
-%%derive equations for fusion of baro
+%derive equations for fusion of baro
 baro_measurement = baro_bias - postion_d;
 H_BARO = jacobian(baro_measurement,state_vector);
 K_BARO = (P*transpose(H_BARO))/(H_BARO*P*transpose(H_BARO) + rbaro);
 [K_BARO,OK_BARO] = OptimiseAlgebra(K_BARO,'OK_BARO');
+
+disp('Complete')
 
 file_name = 'ScriptOutput.mat';
 save(file_name);
