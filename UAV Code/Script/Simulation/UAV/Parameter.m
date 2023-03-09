7775028c761e9e7421688eb66061884427ee8c87
SpindriftXStar <3077448748@qq.com>
2023-03-09 22:40:46 +0800

    Simulation

--- UAV Code/Script/Simulation/UAV/Parameter.m
+++ UAV Code/Script/Simulation/UAV/Parameter.m
@@ -17,6 +17,6 @@ Body_Jxx = 1.663e-2;
 Body_Jyy = 1.663e-2;
 Body_Jzz = 3.033e-2;
 Body_J = [Body_Jxx,0,0;0,Body_Jyy,0;0,0,Body_Jzz];
-Body_G = 9.8;
+Body_G = [0,0,9.8];
 
 THR = (sqrt(Body_Mass*Body_G/4/Rotor_Ct)-Motor_Wb)/Motor_Cr;
